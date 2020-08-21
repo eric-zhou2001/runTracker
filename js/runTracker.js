dd7e110ba94dc3547c0f3cfbc7340873670777d9
ericzhzhou2001@gmail.com <ericzhzhou2001@gmail.com>
2020-08-21 13:25:09 -0400

    Added error checking if user tries to load too many/too little runs

--- js/runTracker.js
+++ js/runTracker.js
@@ -15,13 +15,45 @@ function checkFormInputs() {
 // Loads more runs when requested by the user
 var runCount = 3;
 function loadMoreRuns() {
+    var oldRunCount = runCount;
     runCount += 3;
     // A little strange, but this path is relative from the .php file. I guess it's because we
     // are calling the function from the .php file, so we must be relative to it.
-    $('#run-container').load("phpScripts/loadMoreRuns.php", {
+    $('#run-container').load("phpScripts/loadRuns.php", {
+        newRunCount: runCount
+    });
+    
+    // Sort of a hackey way to do this, since SQL won't throw an error if we try to load more than
+    // the number of existing rows.
+    var newRunCount = $("#run-container > div").length;
+    if (oldRunCount == newRunCount) {
+        event.preventDefault();
+            swal({
+                title: "No more runs to be displayed!",
+                button: "Okay"
+            });
+    }
+}
+
+// Same as above, but loading less runs.
+function loadLessRuns() {
+    var newRunCount = runCount - 3;
+
+    if (newRunCount < 0) {
+        event.preventDefault();
+            swal({
+                title: "No more runs to be displayed!",
+                button: "Okay"
+            });
+            return;
+    }
+
+    runCount -= 3;
+    $('#run-container').load("phpScripts/loadRuns.php", {
         newRunCount: runCount
     });
 }
+
 
 // This is a bit unfortunate since I'm a broke college student who can't afford to give money
 // to Google for their APIs :(
